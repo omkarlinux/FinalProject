cedcc110c51ecf7e0bd51e6b3cffd0f3565f80ff
omkardanke <omkardanke@gmail.com>
2015-12-10 22:38:20 -0700

    Main program initial version built

--- source/test.py
+++ source/test.py
@@ -2,9 +2,17 @@ __author__ = 'omkardanke'
 
 import apiclient
 
-service = apiclient.discovery.build('customsearch','v1')
-customSearchObject = service.cse()
+def main():
+    service = apiclient.discovery.build('customsearch','v1',developerKey='AIzaSyDH0_r3a8C3JJM0j1ofaeh5_3H18trySng')
+    result = service.cse().list(
+        q='IBM',
+        cx='010291719019110963087:n9bkswrylmi'
+    ).execute()
+    print(result)
 
+
+if __name__ == '__main__':
+    main()
 
 
 
