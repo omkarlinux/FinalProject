9a606d3cef40b6e3468659f79af41690cfec69db
omkardanke <omkardanke@gmail.com>
2015-12-11 10:18:58 -0700

    Logic for multiple keywords

--- source/test.py
+++ source/test.py
@@ -6,30 +6,40 @@ import datetime
 import apiclient
 
 def main():
-    readFromGoogle = False;
-    query = 'IBM'
-    devKey = 'AIzaSyDH0_r3a8C3JJM0j1ofaeh5_3H18trySng'
-    customSearchID = '010291719019110963087:n9bkswrylmi'
+    readFromGoogle = False;         #Toggle to fetch results from google or get from already stored file
+    keywordList = ['IBM']                   #Keywords to be searched
+    devKey = open('../documentation/googleCustomSearchAPIKey').read()  #AIzaSyDH0_r3a8C3JJM0j1ofaeh5_3H18trySng'
+    customSearchID = open('../documentation/googleSearchEngineID').read()     #010291719019110963087:n9bkswrylmi'
     if readFromGoogle:
         service = apiclient.discovery.build('customsearch','v1',developerKey=devKey)
-        result = service.cse().list(
-           q=query,
-          cx= customSearchID
-        ).execute()
-        with open('../output/result_' + query + datetime.datetime.now().strftime('%Y%m%d%H%M%S') + '.txt','w') as outfile:
-          json.dump(result,outfile)
+        for keyword in keywordList:
+            #Get search results from google search
+            result = service.cse().list(
+                q= keyword,
+                cx= customSearchID
+                #num= '100'
+                ).execute()
+            with open('../output/result_'
+                              + keyword
+                              + datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+                              + '.txt','w') as outfile:
+                json.dump(result,outfile)
     else:
         outputFiles = os.listdir('../output')
-        filesForQuery = []
-        for filename in outputFiles:
-            if filename.find('*' + query + '*',7,len(filename)-14):
-                filesForQuery.append(filename)
-        filesForQuery.sort(reversed)
-
+        print(outputFiles)
+        for keyword in keywordList:
+            filesForQuery = []
+            for filename in outputFiles:
+                if filename.find( keyword ,7,len(filename)-17) > 0:
+                    print('keyword ' + keyword + ', filename ' + filename)
+                    filesForQuery.append(filename)
+            filesForQuery.sort(reverse=True)
+            if filesForQuery:
+                foundFile = filesForQuery[0]
+                print(foundFile)
+                #result = json.loads(open('../output/' + foundFile).read())
+                #print(result["context"])
 
 
 if __name__ == '__main__':
     main()
-
-
-
