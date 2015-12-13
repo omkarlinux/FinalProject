5ab4690713f77469784d80e860e807737a2822c7
omkardanke <omkardanke@gmail.com>
2015-12-12 17:22:15 -0700

    Added ItemClass and KeywordClass with __init__

--- source/KeywordClass.py
+++ source/KeywordClass.py
@@ -1,21 +1,53 @@
 __author__ = 'omkardanke'
+import apiclient
+import datetime
+import json
+import os
+from .ItemClass import ItemClass
 
 class KeywordClass:
     def __init__(self, inputKeyword):
         self.keyword = inputKeyword
+        self.items = []
+        self.hasLocalResult = False
 
     def getResults(self):
-        devKey = open('../documentation/googleCustomSearchAPIKey').read()  #AIzaSyDH0_r3a8C3JJM0j1ofaeh5_3H18trySng'
-        customSearchID = open('../documentation/googleSearchEngineID').read()     #010291719019110963087:n9bkswrylmi'
-        service = apiclient.discovery.build('customsearch','v1',developerKey=devKey)
-        result = service.cse().list(
-                q= self.keyword,
-                cx= customSearchID
-                #num= '100'
-                ).execute()
-        with open('../output/result_'
-                              + keyword
-                              + datetime.datetime.now().strftime('%Y%m%d%H%M%S')
-                              + '.txt','w') as outfile:
-        json.dump(result,outfile)
+        result = self.getLocalResult()
+        if not self.hasLocalResult:
+            devKey = open('../documentation/googleCustomSearchAPIKey').read()  #AIzaSyDH0_r3a8C3JJM0j1ofaeh5_3H18trySng'
+            customSearchID = open('../documentation/googleSearchEngineID').read()     #010291719019110963087:n9bkswrylmi'
+            service = apiclient.discovery.build('customsearch','v1',developerKey=devKey)
+            result = service.cse().list(
+                    q= self.keyword,
+                    cx= customSearchID
+                    #num= '100'
+                    ).execute()
+            with open('../output/result_'
+                                  + self.keyword
+                                  + datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+                                  + '.txt','w') as outfile:
+                            json.dump(result,outfile)
+
+        for row in result["items"]:
+            itemObject = ItemClass(row["link"])
+            self.items.append(itemObject)       #Initialize Item Class
+
+
+
+    def getLocalResult(self):
+        """
+
+        :rtype : json
+        """
+        outputFiles = os.listdir('../output')
+        filesForQuery = []
+        for filename in outputFiles:
+            if filename.find(self.keyword ,7,len(filename)-17) > 0:
+                filesForQuery.append(filename)
+        filesForQuery.sort(reverse=True)
+        if filesForQuery:
+            foundFile = filesForQuery[0]
+            results = json.loads(open('../output/' + foundFile).read())
+            self.hasLocalResult = True
+            return results
 
--- source/Main.py
+++ source/Main.py
@@ -1,38 +1,16 @@
 __author__ = 'omkardanke'
-import json
-import os
-import datetime
-import apiclient
-#from .ItemClass import ItemClass
-#from .KeywordClass import KeywordClass
+
+from .KeywordClass import KeywordClass
 
 def main():
     readFromGoogle = False;         #Toggle to fetch results from google or get from already stored file
     keywordList = ['IBM']                   #Keywords to be searched
 
     if readFromGoogle:
-        service = apiclient.discovery.build('customsearch','v1',developerKey=devKey)
         for keyword in keywordList:
+            keyword = keyword.capitalize()
             keywordObject = KeywordClass()
-
-    else:
-        outputFiles = os.listdir('../output')
-        for keyword in keywordList:
-            filesForQuery = []
-            for filename in outputFiles:
-                if filename.find( keyword ,7,len(filename)-17) > 0:
-                    filesForQuery.append(filename)
-            filesForQuery.sort(reverse=True)
-            if filesForQuery:
-                foundFile = filesForQuery[0]
-                #print(foundFile)
-                results = json.loads(open('../output/' + foundFile).read())
-                for item in results["items"]:
-                    print(item["link"])
-
-
-
-
+            keywordObject.getResults()
 
 if __name__ == '__main__':
     main()
